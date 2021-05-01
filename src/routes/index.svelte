ca35b04d18de56291958b78baf17f01c6796f0ef
helviti <canfurkankalaycioglu@gmail.com>
2021-05-01 13:37:51 +0300

    init 2

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div name="body" class="flex flex-col h-screen">
+	<div id="header flex-auto">header</div>
+	<div class="flex flex-row items-stretch">
+		<div class="flex-1 h-full">left</div>
+		<div class="flex-1 h-full">right</div>
+	</div>
+</div>
