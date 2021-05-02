035de18bf0e7379914998c37343bb0bf483ac288
Ates Balci <atesbalci@gmail.com>
2021-05-02 21:48:25 +0300

    Implemented multi-page support for Zaytung

--- src/zaytung/zaytung_utils.js
+++ src/zaytung/zaytung_utils.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import matchAll from 'match-all';
 
-const newsUrl = 'https://www.zaytung.com/digerleri.asp';
+const newsUrl = 'https://www.zaytung.com/digerleri.asp?pg=';
 
 function decodeEntities(encodedString) {
     var translate_re = /&(nbsp|amp|quot|lt|gt);/g;
@@ -20,10 +20,19 @@ function decodeEntities(encodedString) {
     });
 }
 
-async function getZaytungNews() {
-    let data = decodeEntities((await axios.get(newsUrl)).data);
+async function getZaytungNewsFromPage(page) {
+    let data = decodeEntities((await axios.get(newsUrl + page)).data);
     let matches = matchAll(data, /style="text-decoration:none;color:black;">([^<>]+)</gi).toArray();
     return matches.slice(1, matches.length);
+}
+
+async function getZaytungNews(pageAmount) {
+    let retVal = [];
+    for(let i = 1; i <= pageAmount; i++) {
+        (await getZaytungNewsFromPage(i)).forEach(element => retVal.push(element));
+    }
+
+    return retVal;
 }
 
 export default getZaytungNews;
