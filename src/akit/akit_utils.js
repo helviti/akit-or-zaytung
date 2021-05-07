043ae295e1b627d9a78dedae8cb9c6eb14ad2ece
Ates Balci <atesbalci@gmail.com>
2021-05-08 02:09:42 +0300

    Implemented Akit link support

--- src/akit/akit_utils.js
+++ src/akit/akit_utils.js
@@ -5,8 +5,15 @@ const newsUrl = 'https://www.yeniakit.com.tr/haber/';
 
 async function getAkitNews() {
     let data = (await axios.get(newsUrl)).data;
-    let matches = matchAll(data, /class="spot">([^<]*)</gi).toArray();
-    return matches.slice(1, matches.length);
+    let matches = matchAll(data, /<div class="element"> <a href="([^"]*)|class="spot">([^<]*)/gi).toArray();
+    let retVal = [];
+    for (let i = 1; i < matches.length; i++) {
+        retVal.push({
+            title: matches[i + 1],
+            link: matches[i]
+        });
+    }
+    return retVal;
 }
 
 export default getAkitNews;
